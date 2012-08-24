150fcad55aa1229ec3871093bf564f804b4ce575
Olivier Refreges <olivier.refreges@gmail.com>
2012-08-24 15:47:02 +0200

    add a simple sed example

--- sed.sh
+++ sed.sh
@@ -1,5 +1,9 @@
 #sed tips
 
+#remove $VAR pattern from $LINE and put result in TOTO
+TOTO=$(echo $LINE | sed "s/$VAR//")
+
+
 #add a new line after searched pattern
 sed "s/<search_pattern>/&\n<newLine>/" input > output
 
@@ -7,3 +11,4 @@ sed "s/<search_pattern>/&\n<newLine>/" input > output
 #doesn't understand \n (for example on HP-UX)
 #y/~/\n/; will replace ~ in replace string bye \n
 sed "s/<search_pattern>/&~<newLine>/; y/~/\n/;" input > output
+
